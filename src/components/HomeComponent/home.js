63a79c1a2df7446967e2d1d5498c44b252569e65
Arvind Kumar <ak8188860205@gmail.com>
2024-04-12 17:29:13 +0530

    Experience updated

--- src/components/HomeComponent/home.js
+++ src/components/HomeComponent/home.js
@@ -7,14 +7,19 @@ export default function Home(){
     return <div className='container-fluid homeBox'>
         <div className='row d-flex align-items-center'>
         <div className='col-lg-6 text-center'>
+            <br />
+            <br />
             <h1>Hi, My name is Arvind Kumar</h1>
             <h1>and I am a passionate</h1>
             <h1>MERN Stack Developer.</h1><br />
             <h3>Skills:-</h3>
-            <p>HTML, CSS, Tailwind css, Bootstrap, Material ui,<br /> Java Script, Api's, React js, Redux Toolkit,<br /> Node js, Express js, Mongodb,<br />DSA, Core JAVA, Git & GitHub</p>
+            <p>HTML, CSS, Tailwind css, Bootstrap, Material ui,<br /> Java Script, Type Script, Api's, React js, Redux Toolkit,<br /> Node js, Express js, Mongodb,<br />DSA, Core JAVA, Git & GitHub</p>
             <br/>
             <button className='btn' onClick={()=>window.open('https://www.linkedin.com/in/arvindkumar2331839/')}>Visit LikedIn</button> &nbsp; &nbsp; 
             <button className='btn' onClick={()=>window.open('https://github.com/Arvind2331839')}>Visit Github</button>
+            <br />
+            <br />
+            <h3>i have one year of experience in mern stack developement from Technorizen software solutions pvt. ltd, Indore, MP</h3><br />
         </div>
         <div className='col-lg-6 text-center'>
             <img src='img/web.png' height={500} style={{borderRadius:'160px', marginTop:60, border:'2px solid white'}}/>
